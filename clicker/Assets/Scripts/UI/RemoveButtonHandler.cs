c2498a053d57959fd23b9c5dad97819a23e42029
Bilguun <soproatlol@gmail.com>
2023-02-20 01:15:48 +0800

    tempo

--- clicker/Assets/Scripts/UI/RemoveButtonHandler.cs
+++ clicker/Assets/Scripts/UI/RemoveButtonHandler.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RemoveButtonHandler : MonoBehaviour
 {
     [SerializeField]
     private BoardManager boardManager;
 
+    private Button selfButton;
+
+    private void Awake()
+    {
+        selfButton = GetComponent(typeof(Button)) as Button;
+    }
+
     public void onClick()
     {
-        boardManager.enterDestoryPhase();
+        // selfButton.navigation
+        if (boardManager.phase == BoardPhases.normal)
+        {
+            boardManager.enterDestoryPhase();
+        }
+        else if (boardManager.phase == BoardPhases.destroy)
+        {
+            boardManager.exitPhases();
+        }
     }
 }
